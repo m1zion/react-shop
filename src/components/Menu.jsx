ecc15e3f399f746b7cc0083eb14ea51a9cc8f2a2
misael <misaelzion@gmail.com>
2022-06-12 23:50:21 -0500

    Agregando los componentes

---
+++ src/components/Menu.jsx
@@ -0,0 +1,21 @@
+import React from React;
+import '../styles/Menu.scss';
+/* CLASE 7 */
+const Menu = () => {
+    return(
+        <div className="desktop-menu">
+        <ul>
+          <li>
+            <a href="/" className="title">My orders</a>
+          </li>    
+          <li>
+            <a href="/">My account</a>
+          </li>    
+          <li>
+            <a href="/">Sign out</a>
+          </li>
+        </ul>
+      </div>
+    );
+}
+export default Menu;
---
+++ src/components/OrderItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../styles/OrderItem.scss';
+const OrderItem = () => {
+    return(   
+        <div className="my-order">
+        <div className="my-order-container">
+          <h1 className="title">My orders</h1>
+          <div className="my-order-content">
+            <div className="order">
+              <p>
+                <span>03.25.21</span>
+                <span>6 articles</span>
+              </p>
+              <p>$560.00</p>
+              <img src="./icons/flechita.svg" alt="arrow"/>
+            </div>           
+          </div>
+        </div>
+      </div>
+    );
+}
+export default OrderItem;
